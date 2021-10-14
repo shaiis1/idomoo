a2d75aac6021287dbc1ca2143a8dccd029589f8b
shayi <shayi@gulliver.co.il>
2021-10-14 09:52:52 +0300

    app routing and heroku fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsFormComponent } from './details-form/details-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: DetailsFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
